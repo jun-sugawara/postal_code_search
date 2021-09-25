d4c755fbdb3038b9e86fc82834e4bcd9ce2fa71a
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-09-25 16:45:36 +0900

    コントローラーにapi取得の記述

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,17 @@
 class SearchController < ApplicationController
+  
   def search
+    if postal_code = params[:postal_code]
+      params = URI.encode_www_form({ziocode: postal_code})
+      uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+      response = Net::HTTP.get.response(uri)
+      result = JSON.parse(response.body)
+      if rezult("rezults")
+        @ripcode = rezult("rezults")[0]["zipcode"]
+        @address1 = rezult("rezults")[0]["address1"]
+        @address2 = rezult("rezults")[0]["address2"]
+        @address3 = rezult("rezults")[0]["address3"]
+      end
+    end
   end
 end
