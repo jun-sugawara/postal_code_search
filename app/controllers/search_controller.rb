b92a6461b7f9a6c5f95c2083e5e75eda3f9bf0c5
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-09-26 12:08:40 +0900

    エラーの修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,15 @@ class SearchController < ApplicationController
   
   def search
     if postal_code = params[:postal_code]
-      params = URI.encode_www_form({ziocode: postal_code})
+      params = URI.encode_www_form({zipcode: postal_code})
       uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
-      response = Net::HTTP.get.response(uri)
+      response = Net::HTTP.get_response(uri)
       result = JSON.parse(response.body)
-      if rezult("rezults")
-        @ripcode = rezult("rezults")[0]["zipcode"]
-        @address1 = rezult("rezults")[0]["address1"]
-        @address2 = rezult("rezults")[0]["address2"]
-        @address3 = rezult("rezults")[0]["address3"]
+      if result["results"]
+        @zipcode = result["results"][0]["zipcode"]
+        @address1 = result["results"][0]["address1"]
+        @address2 = result["results"][0]["address2"]
+        @address3 = result["results"][0]["address3"]
       end
     end
   end
